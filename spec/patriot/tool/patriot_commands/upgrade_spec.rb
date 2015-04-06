c08520cfc59ed260a5a6f968a7f07d7f2c863151
zenmyo_teruyoshi <zenmyo_teruyoshi@cyberagent.co.jp>
2015-04-06 14:44:26 +0900

    fix tests to work without a default configuration file

--- spec/patriot/tool/patriot_commands/upgrade_spec.rb
+++ spec/patriot/tool/patriot_commands/upgrade_spec.rb
@@ -10,13 +10,17 @@ describe Patriot::Tool::PatriotCommands::Upgrade do
 
   describe "upgrade" do
 
+    before :all do
+      @config = File.join($ROOT_PATH, "spec", "config", "test.ini")
+    end
+
     before :each do
       @controller = Patriot::Controller::PackageController.new(config_for_test)
       allow(Patriot::Controller::PackageController).to receive(:new).and_return(@controller)
     end
 
     it "install" do
-      args = ['upgrade']
+      args = ['upgrade', "--config=#{@config}"]
       expect(@controller).to receive(:upgrade)
       expect{Patriot::Tool::PatriotCommand.start(args)}.not_to raise_error
     end
