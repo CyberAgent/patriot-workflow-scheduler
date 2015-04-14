05146a2cb7ab4fb4d17ad8099948315c4ded7f05
zenmyo_teruyoshi <zenmyo_teruyoshi@cyberagent.co.jp>
2015-04-14 20:29:36 +0900

    fix deprecated rspec usage

--- spec/patriot/util/cron_format_parser_spec.rb
+++ spec/patriot/util/cron_format_parser_spec.rb
@@ -6,20 +6,20 @@ describe Patriot::Util::CronFormatParser do
   describe "expand_on_date" do 
     it "should expand hours" do 
       expanded = expand_on_date(Date.new(2013,8,1), "30 * * * *")
-      expanded.should be_a_kind_of Array
-      expanded.size.should == 24
+      expect(expanded).to be_a Array
+      expect(expanded.size).to eq 24
       (0..23).each do |h|
-        expanded.should include DateTime.new(2013,8,1,h,30,0)
+        expect(expanded).to include DateTime.new(2013,8,1,h,30,0)
       end
     end
     
     it "should expand hours and minites" do 
       expanded = expand_on_date(Date.new(2013,8,1), "*/15 * * * *")
-      expanded.should be_a_kind_of Array
-      expanded.size.should == 96
+      expect(expanded).to be_a Array
+      expect(expanded.size).to eq 96
       (0..23).each do |h|
         [0,15,30,45].each do |m|
-          expanded.should include DateTime.new(2013,8,1,h,m,0)
+          expect(expanded).to include DateTime.new(2013,8,1,h,m,0)
         end
       end
     end
@@ -28,42 +28,42 @@ describe Patriot::Util::CronFormatParser do
   describe "target_hours" do 
     it "should return all hours" do 
       hs = target_hours("*")
-      hs.should be_a_kind_of Array
-      hs.size.should == 24
-      (0..23).each{|i| hs.should include i}
+      expect(hs).to be_a Array
+      expect(hs.size).to eq 24
+      (0..23).each{|i| expect(hs).to include i}
     end
 
     it "should handle interval" do 
       hs = target_hours("*/2")
-      hs.should be_a_kind_of Array
-      hs.size.should == 12
-      (0..23).step(2).each{|i| hs.should include i}
+      expect(hs).to be_a Array
+      expect(hs.size).to eq 12
+      (0..23).step(2).each{|i| expect(hs).to include i}
     end
 
     it "should handle range" do 
       hs = target_hours("1-10")
-      hs.should be_a_kind_of Array
-      hs.size.should == 10
-      (1..10).each{|i| hs.should include i}
+      expect(hs).to be_a Array
+      expect(hs.size).to eq 10
+      (1..10).each{|i| expect(hs).to include i}
 
       # with interval
       hs = target_hours("1-10/3")
-      hs.should be_a_kind_of Array
-      hs.size.should == 4
-      [1,4,7,10].each{|i| hs.should include i}
+      expect(hs).to be_a Array
+      expect(hs.size).to eq 4
+      [1,4,7,10].each{|i| expect(hs).to include i}
     end
 
     it "should handle comma separated value" do 
       hs = target_hours("1,2,3,5,8")
-      hs.should be_a_kind_of Array
-      hs.size.should == 5
-      [1,2,3,5,8].each{|i| hs.should include i}
+      expect(hs).to be_a Array
+      expect(hs.size).to eq 5
+      [1,2,3,5,8].each{|i| expect(hs).to include i}
 
       # with range
       hs = target_hours("1-3,5,8")
-      hs.should be_a_kind_of Array
-      hs.size.should == 5
-      [1,2,3,5,8].each{|i| hs.should include i}
+      expect(hs).to be_a Array
+      expect(hs.size).to eq 5
+      [1,2,3,5,8].each{|i| expect(hs).to include i}
     end
   end
 end
