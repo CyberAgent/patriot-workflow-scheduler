83efab27dad141dc6f661b7630c68784c863145c
Takayuki Tanaka <takat007@gmail.com>
2016-04-04 11:44:32 +0900

    Add options to watch files and build production version
    Also change the library to watch files from grunt-este-watch
    to watchfy.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,29 @@ module.exports = function(grunt) {
     pkg : grunt.file.readJSON('package.json'),
     browserify: {
       options: {
+        watch: (function() {
+          if (process.env.DEBUG === 'true') {
+            return true;
+          } else {
+            return false;
+          }
+        }()),
+        keepAlive: (function() {
+          if (process.env.DEBUG === 'true') {
+            return true;
+          } else {
+            return false;
+          }
+        }()),
         transform: ['babelify'],
         browserifyOptions: {
           extensions: ['.jsx','.js']
         }
       },
       dist: {
-       files: {
-         'skel/public/js/patriot-workflow-scheduler-<%= pkg.version %>.js'  : ['src/main/jsx/**/*.jsx']
+        files: {
+          'skel/public/js/patriot-workflow-scheduler-<%= pkg.version %>.js'  : ['src/main/jsx/**/*.jsx']
         }
-      }
-    },
-    env : {
-      development : {
-        NODE_ENV : 'development'
-      },
-      build : {
-        NODE_ENV : 'production'
       }
     },
     uglify: {
@@ -39,22 +45,23 @@ module.exports = function(grunt) {
         src: 'skel/public/views/index.tpl.erb',
         dest:'skel/public/views/index.erb'
       }
-    },
-    esteWatch: {
-      options: {
-        dirs: ['src/main/jsx/**/'],
-        livereload: {
-          enabled: false
-        }
-      },
-      '*': function(filepath) { return 'browserify' }
     }
   });
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-env');
-  grunt.loadNpmTasks('grunt-este-watch');
   grunt.loadNpmTasks('grunt-preprocess');
-  grunt.registerTask('default', ['env:development', 'browserify', 'preprocess']);
-  grunt.registerTask('build', ['env:build', 'browserify', 'uglify', 'preprocess']);
+
+  grunt.registerTask('default', 'default task', function() {
+    console.log('Please set NODE_ENV to production to uglify.');
+    console.log('Please set DEBUG to true to watchify.');
+    console.log('e.g.');
+    console.log('% grunt                      # to build a development version');
+    console.log('% DEBUG=true grunt           # to watch modified files');
+    console.log('% NODE_ENV=production grunt  # to build a production version');
+
+    grunt.task.run('preprocess');
+    grunt.task.run('browserify');
+    if (process.env.NODE_ENV === 'production') grunt.task.run('uglify');
+  });
 }
+
