9e8d54b03e152567360299ddda9c939133e29904
tanak <takat007@gmail.com>
2016-04-05 16:45:39 +0900

    Add uglify and more
    * Add uglify
    
    * Modify not to uglify in development
    
    because it takes time to uglify.
    
    * Add options to watch files and build production version
    
    Also change the library to watch files from grunt-este-watch
    to watchfy to improve performance.
    
    * Remove sourcemap
    
    * Modify not to uglify in development
    
    because it takes time to uglify.
    
    * Add options to watch files and build production version
    
    Also change the library to watch files from grunt-este-watch
    to watchfy to improve performance.
    
    * Remove sourcemap

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,38 +3,55 @@ module.exports = function(grunt) {
     pkg : grunt.file.readJSON('package.json'),
     browserify: {
       options: {
+        watch: grunt.option('watch'),
+        keepAlive: grunt.option('watch'),
         transform: ['babelify'],
         browserifyOptions: {
           extensions: ['.jsx','.js']
         }
       },
       dist: {
-       files: {
-         'skel/public/js/patriot-workflow-scheduler-<%= pkg.version %>.js'  : ["src/main/jsx/**/*.jsx"]
+        files: {
+          'skel/public/js/patriot-workflow-scheduler-<%= pkg.version %>.js'  : ['src/main/jsx/**/*.jsx']
         }
+      }
+    },
+    env: {
+      development: {
+        NODE_ENV: 'development'
+      },
+      production: {
+        NODE_ENV: 'production'
       }
     },
     uglify: {
       options: {
-        sourceMap: true
+        compress: {
+          drop_console: true
+        }
       },
       build: {
         src: 'skel/public/js/patriot-workflow-scheduler-<%= pkg.version %>.js',
         dest: 'skel/public/js/patriot-workflow-scheduler-<%= pkg.version %>.min.js'
       }
     },
-    esteWatch: {
-      options: {
-        dirs: ["src/main/jsx/**/"],
-        livereload: {
-          enabled: false
-        }
-      },
-      '*': function(filepath) { return 'browserify' }
+    preprocess: {
+      html: {
+        src: 'skel/public/views/index.tpl.erb',
+        dest:'skel/public/views/index.erb'
+      }
     }
   });
-  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks("grunt-este-watch");
-  grunt.registerTask('default', ['browserify', 'uglify']);
+  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-preprocess');
+
+  grunt.log.writeln('% grunt                      # to build a development version');
+  grunt.log.writeln('% grunt --watch              # to watch modified files');
+  grunt.log.writeln('% grunt build                # to build a production version');
+
+  grunt.registerTask('default', ['env:development', 'preprocess', 'browserify']);
+  grunt.registerTask('build', ['env:production', 'preprocess', 'browserify', 'uglify']);
 }
+
--- README.md
+++ README.md
@@ -102,6 +102,7 @@ To compile the JSXs, install Nodejs and set up grunt.
 % npm install -g grunt-cli
 % npm install
 % grunt
+( or "grunt --watch" to watchify, "grunt build" to uglify js files. )
 ```
 
 -- Build gem and upgrade installation
